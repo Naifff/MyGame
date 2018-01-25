988bc5344326ddf4e06568e7ac97424bfdafc489
Naif <knifffe@yandex.ru>
2018-01-25 23:57:42 +0300

    libgdx

---
+++ desktop/src/ru/gekbrains/mygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.gekbrains.mygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.gekbrains.mygame.MyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGame(), config);
+	}
+}
