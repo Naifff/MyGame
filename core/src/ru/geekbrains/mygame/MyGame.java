a875bf136c878624421b3ffbf6a63a30e2b0d100
Naif <knifffe@yandex.ru>
2018-01-27 00:35:30 +0300

    картинки+анимация

--- core/src/ru/geekbrains/mygame/MyGame.java
+++ core/src/ru/geekbrains/mygame/MyGame.java
@@ -10,26 +10,39 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 public class MyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture background;
+	Texture ship;
 	TextureRegion region;
-//	int i=0;
+	int i=0;
+	int x=100;
+	int y=100;
+	boolean dest=true;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		background=new Texture("1.png");
+		ship=new Texture("2.png");
 //		region = new TextureRegion(img, (int)(Math.random()*img.getHeight())-100, (int)(Math.random()*img.getWidth())-100,100,100);
 	}
 
 	@Override
 	public void render () {
-//		if(i%10==0){
-//		region = new TextureRegion(img, (int)(Math.random()*img.getHeight())-100, (int)(Math.random()*img.getWidth())-100,100,100);}
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		if(i%10==0){
+		if (dest){
+			x++;
+		}else {x--;}
+		if(x>150){dest=false;}
+		if(x<50){dest=true;}
+		}
+		//		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 //		i++;
 		batch.begin();
-		batch.setColor(0.11f,0.154f,0.241f,1);
-		batch.draw(img, 0, 0);
+		batch.draw(background, 0, 0);
+//		batch.setColor(0.11f,0.154f,0.241f,1);
+		batch.draw(ship, x, y);
 //		batch.draw(region, 100, 100);
 		batch.end();
 	}
