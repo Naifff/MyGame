13d62b08edf059ff202c2f52b4689c62b387cf8e
Naif <knifffe@yandex.ru>
2018-01-27 16:11:27 +0300

    настройка гредла

---
+++ engine/src/ru/geekbrains/mygame/engine/Base2DScreen.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.mygame.engine;
+
+import com.badlogic.gdx.Screen;
+
+public class Base2DScreen implements Screen{
+    @Override
+    public void show() {
+        System.out.println("show");
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        System.out.println("resize");
+    }
+
+    @Override
+    public void pause() {
+        System.out.println("pause");
+    }
+
+    @Override
+    public void resume() {
+        System.out.println("resume");
+    }
+
+    @Override
+    public void hide() {
+        System.out.println("hide");
+    }
+
+    @Override
+    public void dispose() {
+        System.out.println("dispose");
+    }
+}
